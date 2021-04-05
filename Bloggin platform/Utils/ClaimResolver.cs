15b2b1b521295438a157aeff771013fdda59e147
arianjoan <arianproven@gmail.com>
2021-04-05 16:52:05 -0300

    Claim resolver added. Better organization of Services and Controllers. Unit Tests fixed.

---
+++ Bloggin platform/Utils/ClaimResolver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Bloggin_platform.Utils
+{
+    public static class ClaimResolver
+    {
+        public static int? getUserIdFromToken(ClaimsPrincipal userClaims)
+        {
+            var isLogged = userClaims.HasClaim(u => u.Type == "id");
+            var idClaim = string.Empty;
+
+            if (isLogged)
+            {
+                idClaim = userClaims.Claims.First(c => c.Type.Equals("id")).Value;
+                int.TryParse(idClaim, out int id);
+                return id;
+            }
+
+            return null;
+        }
+    }
+}
